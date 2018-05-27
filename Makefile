d5a4fc37ceee484fbc9ab0700b2810836473a4da
Michele <michele.damico1994@gmail.com>
2018-05-27 19:44:26 +0200

    cpp packet sniffing refactoring

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@
 # project subdirectory.
 #
 
-PROJECT_NAME := app-template
+PROJECT_NAME := pds-project
 
 include $(IDF_PATH)/make/project.mk
-
--- main/main.cpp
+++ main/main.cpp
@@ -1,13 +1,3 @@
-/*
- * 1. Open up the project properties
- * 2. Visit C/C++ General > Preprocessor Include Paths, Macros, etc
- * 3. Select the Providers tab
- * 4. Check the box for "CDT GCC Built-in Compiler Settings"
- * 5. Set the compiler spec command to "xtensa-esp32-elf-gcc ${FLAGS} -E -P -v -dD "${INPUTS}""
- * 6. Rebuild the index
-*/
-
-
 #include "freertos/FreeRTOS.h"
 #include "esp_wifi.h"
 #include "esp_wifi_types.h"
@@ -20,17 +10,19 @@
 #include "freertos/task.h"
 #include "sdkconfig.h"
 #include <string>
+#include <string.h>
 #include <map>
 #include <memory>
 #include <stdio.h>
 #include "memory"
 #include <iostream>
-
+#include <iomanip>
+#include "80211Packet.h"
+#include "ProbeReq.h"
 
 #define	LED_GPIO_PIN			GPIO_NUM_4
 #define	WIFI_CHANNEL_MAX		(13)
 #define	WIFI_CHANNEL_SWITCH_INTERVAL	(500)
-#define WIFI_MGMT_PROBE_REQ 0x40
 
 using namespace std;
 
@@ -40,53 +32,10 @@ extern "C" {
 	void app_main(void);
 }
 
-typedef struct {
-	unsigned frame_ctrl:16;
-	unsigned duration_id:16;
-	uint8_t addr1[6]; /* receiver address */
-	uint8_t addr2[6]; /* sender address */
-	uint8_t addr3[6]; /* filtering address */
-	unsigned sequence_ctrl:16;
-} wifi_ieee80211_mac_hdr_t;
-
-typedef struct {
-	//unsigned tag_number:8;
-	//unsigned tag_len:8;
-	uint8_t tag_number;
-	uint8_t tag_len;
-	unsigned char ssid[0];
-} ssid_parameter_set;
-
-typedef struct {
-	wifi_ieee80211_mac_hdr_t hdr;
-
-	ssid_parameter_set ssid_prmtr;
-	uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
-} wifi_ieee80211_packet_t;
-
-typedef struct {
-	uint8_t timestamp;
-	unsigned beacin_interval:16;
-	unsigned capability_info:16;
-
-}wifi_iee80211_mac_body_t;
-
-
-// typedef struct 
-// {
-// 	unsigned:8;
-// } subType;
-
-
-
 static esp_err_t event_handler(void *ctx, system_event_t *event);
 static void wifi_sniffer_init(void);
 static void wifi_sniffer_set_channel(uint8_t channel);
-static const char *wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type);
 static void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type);
-//static void vendor_wifi_sniffer_packet_handler (void *ctx, wifi_vendor_ie_type_t type, const uint8_t sa[6],
-//		const vendor_ie_data_t *vnd_ie, int rssi);
-
 
 void app_main(void) {
 	uint8_t level = 0, channel = 1;
@@ -97,6 +46,7 @@ void app_main(void) {
 
 	ESP_LOGD("prova", "Hello mondo2");
 	ESP_LOGD("sizeof", "%u", sizeof(wifi_ieee80211_mac_hdr_t));
+	
 	/* loop */
 	while (true) {
 		gpio_set_level(LED_GPIO_PIN, level ^= 1);
@@ -104,8 +54,6 @@ void app_main(void) {
 		wifi_sniffer_set_channel(channel);
 		channel = (channel % WIFI_CHANNEL_MAX) + 1;
     	}
-	map <string,string> map;
-	shared_ptr<string> ptr = make_shared<string>("cane");
 }
 
 esp_err_t
@@ -129,36 +77,13 @@ wifi_sniffer_init(void)
     	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
     	ESP_ERROR_CHECK( esp_wifi_start() );
 	esp_wifi_set_promiscuous(true);
-    //esp_wifi_set_vendor_ie(true, WIFI_VND_IE_TYPE_PROBE_REQ, 1, NULL);
-	//esp_wifi_set_vendor_ie_cb(&vendor_wifi_sniffer_packet_handler, NULL);
 	esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
 }
 
 void
 wifi_sniffer_set_channel(uint8_t channel)
 {
-
-	esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
-}
-
-const char *
-wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type)
-{
-	switch(type) {
-	case WIFI_PKT_MGMT: return "MGMT";
-	case WIFI_PKT_DATA: return "DATA";
-	default:
-	case WIFI_PKT_MISC: return "MISC";
-	}
-}
-
-string
-wifi_sniffer_packet_subtype2str(uint8_t subtype) {
-	switch (subtype) {
-		case WIFI_MGMT_PROBE_REQ: return string("PROBE REQ");
-		default: 
-			return string("");
-	}
+	ESP_ERROR_CHECK(esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE));
 }
 
 void
@@ -168,67 +93,45 @@ wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
 	if (type != WIFI_PKT_MGMT) {
 		return;
 	}
+	char buf[32 + 1];
 	uint8_t subtype;
-	//printf("%u", type);
-	//memcpy(subtype, buff + 0x30, 1);
-//	subtype = (unsigned char*)buff + 0x1C;
-//	printf("porcaccio gesu: %02x", *subtype);
-//	if (*subtype == 0x40) {
-//		printf("leskere");
-//	}
-//	for (int i = 0; i < 100; i++) {
-//		subtype = (unsigned char*) buff + i;
-//		printf("%02x", *subtype);
-//		if (i % 16 == 0)
-//			printf("\n");
-//	}
+	int8_t rssi;
+	uint8_t channel;
+	uint8_t ssidLen; 
+
+	string ssid;
+	array<uint8_t,6> sAddr;
+	array<uint8_t,6> dAddr;
+	array<uint8_t,6> bssid;
+	
 
 	const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
 	const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
 	const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
 
+	/* filter only PROBE REQ packets */
 	subtype = hdr->frame_ctrl;
+	subtype = subtype & 0xF0;
+
 	if (subtype != 0x40) {
 			return;
 	}
 
-	uint8_t len = ipkt->ssid_prmtr.tag_len;
+	ssidLen = ipkt->ssid_prmtr.tag_len;
+	memcpy(buf, ipkt->ssid_prmtr.ssid, ssidLen);
 
-	//cout << len;
-	cout << (int)ipkt->ssid_prmtr.tag_number << " " << (int)ipkt->ssid_prmtr.tag_len << "\n"; 
-	for (uint8_t i = 0; i < len; i++){
-		cout << ipkt->ssid_prmtr.ssid[i];
-	}
-	//const wifi_beacon_payload *bpay = ipkt->payload;
-	cout << "SSID= " << wifi_sniffer_packet_subtype2str(subtype);
-	//cout << hex << hdr->frame_ctrl;
-	printf("PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
-		" ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
-		" ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
-		" BSSID=%02x:%02x:%02x:%02x:%02x:%02x \n",
-		wifi_sniffer_packet_type2str(type),
-		ppkt->rx_ctrl.channel,
-		ppkt->rx_ctrl.rssi,
-		/* ADDR1 */
-		hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
-		hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
-		/* ADDR2 */
-		hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
-		hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
-		/* ADDR3 */
-		hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
-		hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
-	);
+	buf[ssidLen] = '\0';
+	ssid = buf;
+	rssi = ppkt->rx_ctrl.rssi;
+	channel = ppkt->rx_ctrl.channel;
+	channel &= 0xF0;
+	
+	copy(begin(hdr->addr1), end(hdr->addr1), begin(sAddr));
+	copy(begin(hdr->addr2), end(hdr->addr2), begin(dAddr));
+	copy(begin(hdr->addr3), end(hdr->addr3), begin(bssid));
+	
+	ProbeReq probe(type, subtype, ppkt->rx_ctrl.channel,
+		rssi, ssid, ssidLen, sAddr, dAddr, bssid);
 
+	cout << probe;
 }
-
-// void
-// vendor_wifi_sniffer_packet_handler (void *ctx, wifi_vendor_ie_type_t type, const uint8_t sa[6],
-// 		const vendor_ie_data_t *vnd_ie, int rssi) {
-// 	printf("topp");
-//}
-
-
-
-
-
